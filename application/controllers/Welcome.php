5485100de8f54a2f39626fbbfe20873e2aad3325
17191371munir <17191371m.sirozulmunir@gmail.com>
2022-03-26 08:19:46 +0700

    project contoh1

---
+++ application/controllers/Welcome.php
@@ -0,0 +1,17 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Welcome extends CI_Controller {
+
+	
+	public function index()
+	{
+		echo "<h1>Perkenalkan</h1>";
+ echo"Nama saya M.Sirozul Munir
+<br> Saya tingga di daerah rawa lele<br>
+ hobi saya mancing
+ <br>saya lahir di tegal 
+ <br>olah raga yang saya sukai adalah
+ Bulutangkis";
+	}
+}
